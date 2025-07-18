ab2f5a88c9b42cad279963dc8b791f28cc5265f7
Subhajit Das <subhajit9004@gmail.com>
2025-07-18 23:20:37 +0530

    Add files via upload

---
+++ 5_Practice.cpp
@@ -0,0 +1,16 @@
+// Area  of a Square.....
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n , ar ;
+
+    cout<< " Enter  a side of Square :" <<endl;
+    cin >> n;
+
+    ar = n * n ;
+    cout << "Area  of a Square :" << ar << endl;
+
+    return 0;
+}
