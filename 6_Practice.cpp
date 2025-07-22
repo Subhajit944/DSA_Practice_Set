ff100033adf5409f0d312c09f20c4edd0057629b
Subhajit Das <subhajit9004@gmail.com>
2025-07-22 22:18:07 +0530

    Add files via upload

---
+++ 6_Practice.cpp
@@ -0,0 +1,20 @@
+// find sum of 2 numbers.....
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a, b, s;
+
+    cout << "Enter the first number(a)"<<endl;
+    cin >> a;
+
+    cout << "Enter the second number(b)"<<endl;
+    cin >> b;
+
+    s = a + b;
+
+    cout << "Sub of two numbers :" << s <<endl;
+    return 0;
+
+}
