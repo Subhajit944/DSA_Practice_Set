16e8d849039345301b78914e6aaff6cc5df2634e
Subhajit Das <subhajit9004@gmail.com>
2025-07-31 23:26:30 +0530

    Add files via upload

---
+++ 10_Practice.cpp
@@ -0,0 +1,18 @@
+// Arithmetic (+ , - , * , / , % ).....
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    int a = 10, b = 5;
+
+    cout <<" sum = " << (a+b) << endl;
+    cout <<" difference = " << (a-b) << endl;
+    cout <<" product = " << (a*b) << endl;
+    cout <<" divition = " << (a/b) << endl;
+    cout <<" modulo = " << (a%b) << endl;
+
+    return 0;
+
+}
