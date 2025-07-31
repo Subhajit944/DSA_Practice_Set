d6da9263275518411c0b25ada7a2b83fe30170f4
Subhajit Das <subhajit9004@gmail.com>
2025-07-31 23:26:43 +0530

    Add files via upload

---
+++ 11_Practice.cpp
@@ -0,0 +1,18 @@
+//Relational  
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+    cout << (3 < 5) << endl;
+    cout << (3 > 5) << endl;
+    cout << (3 <= 5) << endl;
+    cout << (3 >= 5) << endl;
+    cout << (3 == 5) << endl;
+    cout << (3 != 5) << endl;
+
+
+    return 0;
+}
