6c970ea76f9879bd1eb62950a71e24610344b7f9
Subhajit Das <subhajit9004@gmail.com>
2025-08-02 20:38:15 +0530

    Add files via upload

---
+++ 12_Practice.cpp
@@ -0,0 +1,19 @@
+// Sum of 2 numbers...
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a , b;
+
+    cout << "Enter a : " ;
+    cin >> a;
+    cout << "Enter b : " ;
+    cin >> b;
+
+    int sum = a + b;
+
+    cout << "sum =  " << sum << endl;
+    return 0;
+    
+}
